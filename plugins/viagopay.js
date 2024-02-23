2b8d574f213894448ca25a8bcaadb52135e244fe
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-23 12:47:25 +0700

    viagopay.js

--- plugins/viagopay.js
+++ plugins/viagopay.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 	conn.reply(m.chat, `╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via Gopay
 ╠➥ Ke nomer ini
-║➥ *TANYA KE OWNER*
+║➥ 085794908894
 ║
 ╠═〘 Fitur 〙 ═
 ║
@@ -22,8 +22,8 @@ let handler = async (m, { conn }) => {
 ╠➥ *DAN 200 LEBIH*
 ║   *FITUR LAINNYA*
 ║ 
-╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
+╠═ ©2024 Kιɳα Bσƚ
+╠═ Iky Official
 ╠═〘 ${namabot} 〙 ═`.trim(), m)
 }
 
