86c636220ee17e21d49ab138652f1d79a1817582
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-23 23:48:43 +0700

    index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Krizyn', {
+CFonts.say('Iky Official', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
