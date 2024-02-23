99e09928291989746cc5a1b55b69bcaf22dc4536
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-23 12:46:01 +0700

    viadana.js

--- plugins/viadana.js
+++ plugins/viadana.js
@@ -2,17 +2,17 @@ let handler = async (m, { conn }) => {
 	conn.reply(m.chat, `╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via DANA
 ╠➥ Ke nomer ini
-║➥ 081360482998
+║➥ 085794908894
 ║
 ╠═〘 Fitur 〙 ═
 ║
 ╠➥ *FITUR DOWNLOADER 500++
 ║     YouTube, Twitter, 
 ║     Tiktok, Instagram, dll.
-║    *FITUR STIKER*
+║   *FITUR STIKER*
 ╠➥ *FITUR ADMIN*
-║    *KICK ORANG*
-║    *JADIIN ADMIN*
+║   *KICK ORANG*
+║   *JADIIN ADMIN*
 ╠➥ *FITUR ISLAM*
 ║   *QURAN*
 ║   *CEK JADWAL SHALAT*
@@ -22,8 +22,8 @@ let handler = async (m, { conn }) => {
 ╠➥ *DAN 200 LEBIH*
 ║   *FITUR LAINNYA*
 ║ 
-╠═ ©2021 Rpg wabot-aq
-╠═ Scrip original by Nurutomo
+╠═ ©2024 Kιɳα Bσƚ
+╠═ Iky Official
 ╠═〘 ${namabot} 〙 ═`.trim(), m)
 }
 
