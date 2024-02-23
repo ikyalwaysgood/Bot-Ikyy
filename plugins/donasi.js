25b739988c142eff39be63b075abf2754a443b39
Ikyofficial <155772050+ikyalwaysgood@users.noreply.github.com>
2024-02-23 12:24:46 +0700

    donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,13 +1,13 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + teks)).buffer(), `
-*Allo mypren bantu donasi yuk supaya bot aktif selalu👋*
+*Hi bantu donasi yuk supaya bot aktif selalu👋*
 ╔═══════════════════
-║ _*DONASI UNTUK*_  𝙆𝙧𝙞𝙯𝙮𝙣 ×͜×
+║ _*DONASI DI SINI*_ 
 ╠═══════════════════
 ║╭──❉ 〔 *𝗗𝗢𝗡𝗔𝗦𝗜* 〕 ❉──────
-║│➸ *DANA* : 081360482998
-║│➸ *PULSA*: 081360482998
-║│➸ *GOPAY*: Lagi Gak pakai
+║│➸ *DANA* : 085794908894
+║│➸ *PULSA*: 083164864536
+║│➸ *GOPAY*: 085794908894
 ║╰──────────────────
 ╰═══════════════════
 
